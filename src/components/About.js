767de95336474bdac7ab2ee28fcb1ecf409c06fa
TrojinCat <kajarrod@gmail.com>
2019-06-05 22:25:54 +1000

    Added in react-player for the audio control

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,24 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
-const About = () => {
-  return <div>Some stuff</div>;
-};
+class About extends React.Component {
+  state = {
+    playing: false,
+    controls: true
+  };
+
+  render() {
+    const { playing, controls } = this.state;
+    return (
+      <div>
+        <ReactPlayer
+          url="https://pinecast.com/listen/46030d36-3db8-4521-9177-a21b76128acd.mp3"
+          playing={playing}
+          controls={controls}
+        />
+      </div>
+    );
+  }
+}
 
 export default About;
