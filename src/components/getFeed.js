5fa251545524a6a57f3685a25e94fb580521d558
TrojinCat <kajarrod@gmail.com>
2019-06-09 17:01:58 +1000

    Input RSS into object and display

--- src/components/getFeed.js
+++ src/components/getFeed.js
@@ -1,38 +1,40 @@
 import React from "react";
 import axios from "axios";
-import xml2js from "xml2js";
+//import xml2js from "xml2js";
 
 class getFeed extends React.Component {
   state = {
-    rssFeed: [],
+    episodes: null,
     URL: "https://pinecast.com/feed/end-of-the-reel"
   };
 
   getFeed() {
     var parseString = require("xml2js").parseString;
-
-    //var xml = "<root>Hello xml2js!</root>"
-    //parseString(xml, function (err, result) {
-    //  console.dir(result);
-
     let URL = this.state.URL;
 
-    axios.get(`${URL}`).then(response => {
-      //console.log(response.data);
-      parseString(response.data, function(err, result) {
-        console.log(result.rss.channel[0]);
-        this.setState({ rssFeed: result.rss.channel[0] });
-      });
-      //  this.setState({ rssFeed: response.data });
-    });
+    axios.get(`${URL}`).then(response =>
+      parseString(response.data, (err, result) => {
+        const episodes = result.rss.channel[0].item;
+
+        this.setState({ episodes: episodes });
+        console.log(this.state.episodes);
+      })
+    );
   }
 
+  displayTest = () => {
+    if (this.state.episodes === null) {
+      return <div>Enter some dates to find out what is best to wear</div>;
+    } else {
+      return <li>{JSON.stringify(this.state.episodes[0].title)}</li>;
+    }
+  };
+
   render() {
-    const { rssFeed } = this.state;
     return (
       <div>
         <button onClick={() => this.getFeed()}>Click</button>
-        <div>RSS FEED</div>
+        <div>{this.displayTest()}</div>
       </div>
     );
   }
